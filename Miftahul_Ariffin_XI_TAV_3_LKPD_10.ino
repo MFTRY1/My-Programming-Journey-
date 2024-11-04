6ad3208215eccbef43a7f245526041f429e91710
MFTRY1 <miftahulariffin637@gmail.com>
2024-11-04 14:53:17 +0700

    Update Miftahul_Ariffin_XI_TAV_3_LKPD_10.ino

--- Miftahul_Ariffin_XI_TAV_3_LKPD_10.ino
+++ Miftahul_Ariffin_XI_TAV_3_LKPD_10.ino
@@ -4,7 +4,7 @@ void setup() {
     Serial.begin(9600);
 }
 void loop() {
-    int buttonState = digitalRead(buttonPin);
+   int buttonState = digitalRead(buttonPin);
    Serial.print("Tombol ditekan: ");
    if (buttonState == HIGH) {
      Serial.println("YA");
